f592bfaa3a5ef2d85f5d4a674a0f2773e1524a1c
Devbrat Srivastava <devbrat0srivastava@gmail.com>
2020-10-29 23:03:23 +0530

    Create ZMatrix.java

---
+++ ZMatrix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Z
+{
+    public static void main(String  args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n-1;i++)
+        System.out.print(a[0][i]+" ");
+        for(int i=0;i<n;i++)
+        System.out.print(a[i][n-1-i]+" ");
+        for(int i=1;i<n;i++)
+        System.out.print(a[n-1][i]+" ");
+    }
+}
